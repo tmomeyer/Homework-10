c191acfdbff132432f12e3b7a860e414395ca5f9
Trevor Momeyer <t.momeyer@setonhill.edu>
2020-04-23 11:18:39 -0400

    Initial Commit

---
+++ store and save information/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  store and save information
+//
+//  Created by Trevor Momeyer on 4/23/20.
+//  Copyright © 2020 Trevor Momeyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ store and save informationTests/store_and_save_informationTests.swift
@@ -0,0 +1,34 @@
+//
+//  store_and_save_informationTests.swift
+//  store and save informationTests
+//
+//  Created by Trevor Momeyer on 4/23/20.
+//  Copyright © 2020 Trevor Momeyer. All rights reserved.
+//
+
+import XCTest
+@testable import store_and_save_information
+
+class store_and_save_informationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
