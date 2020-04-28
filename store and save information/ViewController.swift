9e2b7b94c1637eadd1b341d3211cd3a6b12bf0a0
Trevor Momeyer <t.momeyer@setonhill.edu>
2020-04-28 11:44:09 -0400

    Homework 10

--- store and save information/ViewController.swift
+++ store and save information/ViewController.swift
@@ -9,6 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var drinkPref: UITextField!
+    @IBOutlet weak var results: UITextView!
+    @IBOutlet weak var nickName: UITextField!
+    
+    
+    
+    @IBAction func saveResults(_ sender: Any) {
+        let csvLine:String = "\(firstName.text!),\(lastName.text!),\(drinkPref.text!),\(nickName.text!)\n"
+        let paths =
+            NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory,FileManager.SearchPathDomainMask.userDomainMask, true)
+        let docDir:String=paths[0]
+        let resultsFile:String=(docDir as NSString).appendingPathComponent("results.csv")
+        
+        if !FileManager.default.fileExists(atPath: resultsFile) {
+            FileManager.default.createFile(atPath: resultsFile,contents: nil, attributes: nil)
+        }
+        
+        let fileHandle: FileHandle=FileHandle(forUpdatingAtPath:resultsFile)!
+        fileHandle.seekToEndOfFile()
+        fileHandle.write(csvLine.data(using: String.Encoding.utf8)!)
+        fileHandle.closeFile()
+        
+        firstName.text = ""
+        lastName.text = ""
+        drinkPref.text = ""
+    }
+    
+    
+    
+    
+    @IBAction func displayResults(_ sender: Any) {
+        let paths =
+        NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory,FileManager.SearchPathDomainMask.userDomainMask, true)
+        let docDir:String=paths[0] as String
+        
+        let resultsFile:String=(docDir as NSString).appendingPathComponent("results.csv")
+        
+        
+        
+        if FileManager.default.fileExists(atPath: resultsFile){
+            let fileHandle:FileHandle = FileHandle(forReadingAtPath:resultsFile)!
+            let resultsData:String!=NSString(data: fileHandle.availableData, encoding: String.Encoding.utf8.rawValue)! as String
+            fileHandle.closeFile()
+            
+            results?.text=resultsData
+        }
+        else
+        {
+            
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
